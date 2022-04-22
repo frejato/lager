7295d13dc8cc6b4859f5aec65638dbf0615ce64c
Freja Tollebäck <frtl18@student.bth.se>
2022-04-22 21:09:33 +0100

    Added style and fonts

--- App.tsx
+++ App.tsx
@@ -1,16 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { Image, StyleSheet, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import AppLoading from 'expo-app-loading';
+import { useFonts, Anton_400Regular } from '@expo-google-fonts/anton';
 import Stock from './components/Stock.tsx';
 import warehouse from './assets/warehouse.jpg';
+// import { useFonts } from 'expo-font';
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Anton_400Regular,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.container}>
-        <Text style={{ color: '#fff', fontSize: 42 }}>Lager-Appen</Text>
-        <Image source={warehouse} style={{ width: 320, height: 240 }} />
+      <View style={styles.base}>
+        <Text style={styles.heading}>Lager-Appen</Text>
+        <Image source={warehouse} style={styles.image} />
         <Stock />
+        <Text style={styles.footer}>Freja 2022 - BTH</Text>
         <StatusBar style="auto" />
       </View>
     </SafeAreaView>
@@ -18,13 +30,34 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
+  heading: {
+    color: '#fff',
+    fontSize: 42,
+    paddingBottom: 24,
+    paddingTop: 24,
+    fontFamily: 'Anton_400Regular',
+    textShadowColor: '#000',
+    textShadowRadius: 10,
+    textShadowOffset: { width: 2, height: 2 },
+  },
+  image: {
+    width: 300,
+    height: 180,
+    borderRadius: 10,
+  },
   container: {
     flex: 1,
-    backgroundColor: '#23395B',
-    paddingLeft: 12,
-    paddingRight: 12,
   },
   base: {
     flex: 1,
+    alignItems: 'center',
+    textAlign: 'center',
+    backgroundColor: '#2E4052',
+    paddingLeft: 12,
+    paddingRight: 12,
   },
+  footer: {
+    color: '#BDD9BF',
+    paddingTop: 18,
+  }
 });
--- components/Stock.tsx
+++ components/Stock.tsx
@@ -1,6 +1,25 @@
 import { useState, useEffect } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import { useFonts, Lato_400Regular } from '@expo-google-fonts/lato';
 import config from "../config/config.json";
+
+export default function Stock() {
+    let [fontsLoaded] = useFonts({
+        Lato_400Regular,
+    });
+
+    if (!fontsLoaded) {
+        return <AppLoading />;
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.heading}>Lagerförteckning</Text>
+            <StockList />
+        </View>
+    );
+}
 
 function StockList() {
     const [products, setProducts] = useState([]);
@@ -11,19 +30,53 @@ function StockList() {
             .then(result => setProducts(result.data));
     }, []);
 
-    const list = products.map((product, index) => <Text key={index}>{product.name}</Text>);
+    const list = products.map((product, index) => <Text style={styles.list} key={index}>{product.name} - <Text style={styles.amount} key={index}>{product.stock}</Text> st</Text>);
 
     return (
-        <View>
+        <View style={styles.base}>
+            <Text style={styles.info}>Namn - Saldo</Text>
             {list}
         </View>
     );
 }
 
-export default function Stock() {
-    return (
-        <Text style={{ color: '#fff', fontSize: 24 }}>Lagerförteckning</Text>
 
-    )
-}
 
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#412234',
+        width: 300,
+        alignItems: 'center',
+        paddingTop: 24,
+        paddingBottom: 24,
+        borderRadius: 10,
+        marginTop: 24,
+    },
+    heading: {
+        color: '#fff',
+        fontSize: 24,
+        fontFamily: 'Lato_400Regular',
+    },
+    list: {
+        fontFamily: 'Lato_400Regular',
+        color: '#fff',
+        fontSize: 16,
+        paddingLeft: 15,
+        paddingTop: 6,
+    },
+    info: {
+        textAlign: 'center',
+        fontFamily: 'Lato_400Regular',
+        alignItems: 'center',
+        color: '#fff',
+        fontSize: 16,
+        textDecorationStyle: 'solid',
+        textDecorationColor: '#fff',
+        textDecorationLine: 'underline',
+        paddingTop: 12,
+    },
+    amount: {
+        fontWeight: 'bold',
+        color: '#ffc857',
+    }
+});
